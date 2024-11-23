a501a260445440eedc7b7d709ca61f7a353d294a
Piyush Bhavsar~ <piyushbhavsar.you@gmail.com>
2024-11-24 00:58:33 +0530

    main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 '''
 It will create an instance of the Flask class,
 which will be your WSGI (Web Server Gatway Interface) application
@@ -9,11 +9,11 @@ app=Flask(__name__)
 
 @app.route("/")
 def welcome():
-    return "Welcome to the flask"
+    return "<html><H1>Welcome to the flask</H1></html>"
 
 @app.route("/index")
 def index():
-    return "Welcome to the index page"
+    return render_template('index.html')
 
 
 if __name__=="__main__":
