f211bc2191f137a225e628ef7c0b502883a2c0ec
Piyush Bhavsar~ <piyushbhavsar.you@gmail.com>
2024-11-25 17:20:20 +0530

    main.py

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ def welcome():
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 
 if __name__=="__main__":
     app.run(debug=True)
