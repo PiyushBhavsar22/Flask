3f6927de20c0e6d735fdec8151252a474a703403
Piyush Bhavsar~ <piyushbhavsar.you@gmail.com>
2024-11-26 14:22:16 +0530

    jinja.py

--- jinja.py
+++ jinja.py
@@ -1,5 +1,5 @@
 ### Building Url Dynamically
-## Variable Ru;e
+## Variable Rule
 ### Jinja 2 Template Engine
 
 ### Jinja2 Template Engine
@@ -9,19 +9,17 @@
 {#...#} this is for comments
 '''
 
-
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect,url_for
 '''
-It will create an instance of the Flask class,
-which will be your WSGI (Web Server Gatway Interface) application
+ It creates an instance of the Flask class, 
+ which will be your WSGI (Web Server Gateway Interface) application.
 '''
-
 ###WSGI Application
 app=Flask(__name__)
 
 @app.route("/")
 def welcome():
-    return "<html><H1>Welcome to the flask</H1></html>"
+    return "<html><H1>Welcome to the flask course</H1></html>"
 
 @app.route("/index",methods=['GET'])
 def index():
@@ -32,12 +30,6 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/submit',methods=['GET','POST'])
-def submit():
-    if request.method=='POST':
-        name=request.form['name']
-        return f'Hello {name}!'
-    return render_template('form.html')
 
 ## Variable Rule
 @app.route('/success/<int:score>')
@@ -48,8 +40,7 @@ def success(score):
     else:
         res="FAILED"
 
-    return render_template('result.html', results=res)
-
+    return render_template('result.html',results=res)
 
 ## Variable Rule
 @app.route('/successres/<int:score>')
@@ -59,16 +50,36 @@ def successres(score):
         res="PASSED"
     else:
         res="FAILED"
-
+    
     exp={'score':score,"res":res}
 
-    return render_template('result1.html', results=exp)
+    return render_template('result1.html',results=exp)
 
-## if condition
-@app.route('/successif/<int:score>')
+## if confition
+@app.route('/sucessif/<int:score>')
 def successif(score):
 
-    return render_template('result.html', results=score)
+    return render_template('result.html',results=score)
+
+@app.route('/fail/<int:score>')
+def fail(score):
+    return render_template('result.html',results=score)
+
+@app.route('/submit',methods=['POST','GET'])
+def submit():
+    total_score=0
+    if request.method=='POST':
+        science=float(request.form['science'])
+        maths=float(request.form['maths'])
+        c=float(request.form['c'])
+        data_science=float(request.form['datascience'])
+
+        total_score=(science+maths+c+data_science)/4
+    else:
+        return render_template('getresult.html')
+    return redirect(url_for('successres',score=total_score))
+            
+        
 
 
 
